528efe07a40698dc30e9ae0c8e403b0e58afefa3
ParkSeonIk <parksimis@gmail.com>
2021-03-17 15:49:16 +0900

    Fear : Add session code 장고 세션 사용을 위한 session code 삽입

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('login/', crawling, name='crawling'),
+    path('login/crawltable', crawl_table, name='crawl_table')
 ]
