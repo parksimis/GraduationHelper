2e14aa7077f8972f31a0649401aa799eda3da341
ParkSeonIk <parksimis@gmail.com>
2021-03-17 15:52:20 +0900

    Feat : Add crawl data func 사용자 학점 테이블 크롤링 함수 구현

--- engine.py
+++ engine.py
@@ -28,14 +28,13 @@ def login_check(response, session):
     else:
         return False
 
-
 def get_html(url, session):
     '''
     URL 주소를 받아서, html 페이지를 scrapping 하는 method
     ========================================================
     input :
     url -> 원하는 페이지 url 주소
-    scrapping -> True 시 html tag scrapping(default=True)
+    session -> 유지되는 session
     output : html -> 해당 주소의 html 페이지
     '''
     response = session.get(url)
@@ -43,16 +42,6 @@ def get_html(url, session):
 
     html = BeautifulSoup(response.text, "html.parser")
     return html
-
-
-def get_user_info(session):
-    html = get_html('https://kutis.kyonggi.ac.kr:443/webkutis/view/hs/wshj1/wshj111s.jsp?submenu=1&m_menu=wsco1s02&s_menu=wshj111s', session)
-
-    department = get_user_department(html)
-    foreigner = check_foreigner(html)
-    return department, foreigner
-
-
 
 def get_user_department(html):
 
@@ -72,3 +61,32 @@ def check_foreigner(html):
         return True
     else:
         return False
+
+def get_user_info(session):
+    html = get_html('https://kutis.kyonggi.ac.kr:443/webkutis/view/hs/wshj1/wshj111s.jsp?submenu=1&m_menu=wsco1s02&s_menu=wshj111s', session)
+
+    department = get_user_department(html)
+    foreigner = check_foreigner(html)
+    return department, foreigner
+
+
+def crawl_table(session):
+    html = get_html('http://kutis.kyonggi.ac.kr/webkutis/view/hs/wssj1/wssj170s.jsp?submenu=2', session)
+
+    thead = html.select_one('thead')  # 테이블 box 가져오기
+    tr = thead.select('tr')  # tr만 뽑아오기
+
+    table = []
+    for i in range(len(tr)):
+        row = tr[i].text.strip().split('\n')
+
+        if len(row) == 9:  # 총 칼럼이 9개인 것은 정상적인 것이므로 제외
+            table.append(row)
+
+        elif len(row) == 7:  # 칼럼이 7개인 경우에는 앞에 두개 넣어줌
+            row = [table[-1][0], " "] + row
+            table.append(row)
+    hakjeom = pd.DataFrame(table[1:], columns=table[0])
+    hakjeom = hakjeom[hakjeom['유효구분'] == '유효']
+
+    return hakjeom
--- main/views.py
+++ main/views.py
@@ -8,23 +8,37 @@ def index(request):
     return render(request, 'main/index.html')
 
 def crawling(request):
+
     user_id = request.POST.get('user_id')
     user_pwd = request.POST.get('user_pwd')
-    print(user_id, user_pwd)
+
+    request.session['user_id'] = user_id
+    request.session['user_pwd'] = user_pwd
 
     user_response, session = engine.login(user_id, user_pwd)
+
     if engine.login_check(user_response, session):
-        print('OK')
         department, foreigner = engine.get_user_info(session)
-        print(department, foreigner)
+        # request.session['sessions'] = session
         context = {
             'department': department,
             'foreigner': foreigner
         }
         return render(request, 'main/user_detail.html', context)
     else:
+        del request.session['user_id']
+        del request.session['user_pwd']
         return render(request, 'main/login_fail.html')
 
 
+def crawl_table(request):
+    user_id = request.session['user_id']
+    user_pwd = request.session['user_pwd']
+    user_response, session = engine.login(user_id, user_pwd)
+
+    tt = engine.crawl_table(session)
+    print(tt)
+
+    return render(request, 'main/user_detail.html')
 
 
