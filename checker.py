6c178faf0d84ea318168ee48688b54a6d6e3aede
ziho9593 <no1hjyo@naver.com>
2021-04-01 20:29:41 +0900

    Feat : Add course_2018() for checing 18, 19, 20 18, 19, 20 학번 진단을 위한 2018 교육과정 반영

--- checker.py
+++ checker.py
@@ -27,7 +27,7 @@ def course_2012(major_1, major_2, foreigner, records, maj_dict):
                 elif major_2 != False and div[5:-1] in major_2: # 복수전공
                     maj_r2 += 1
                     maj_2 += int(sbj['학점'])
-                    tatal += int(sbj['학점'])
+                    total += int(sbj['학점'])
                 else: # 기타
                     cul += int(sbj['학점'])
                     total += int(sbj['학점'])
@@ -42,7 +42,7 @@ def course_2012(major_1, major_2, foreigner, records, maj_dict):
                 elif major_2 != False and div[5:-1] in major_2: # 복수전공
                     maj_c2 += 1
                     maj_2 += int(sbj['학점'])
-                    tatal += int(sbj['학점'])
+                    total += int(sbj['학점'])
                 else: # 기타
                     cul += int(sbj['학점'])
                     total += int(sbj['학점'])
@@ -104,7 +104,7 @@ def course_2017(major_1, major_2, foreigner, records, maj_dict):
                 elif major_2 != False and div[5:-1] in major_2: # 복수전공
                     maj_r2 += 1
                     maj_2 += int(sbj['학점'])
-                    tatal += int(sbj['학점'])
+                    total += int(sbj['학점'])
                 else: # 기타
                     cul += int(sbj['학점'])
                     total += int(sbj['학점'])
@@ -116,7 +116,86 @@ def course_2017(major_1, major_2, foreigner, records, maj_dict):
                 elif major_2 != False and div[5:-1] in major_2: # 복수전공
                     maj_c2 += 1
                     maj_2 += int(sbj['학점'])
-                    tatal += int(sbj['학점'])
+                    total += int(sbj['학점'])
+                else: # 기타
+                    cul += int(sbj['학점'])
+                    total += int(sbj['학점'])
+            elif sbj['이수구분'] == maj_dict[major_1][0]:
+                maj_1 += int(sbj['학점'])
+                total += int(sbj['학점'])
+            elif major_2 != False and sbj['이수구분'] == maj_dict[major_2][0]:
+                maj_2 += int(sbj['학점'])
+                total += int(sbj['학점'])
+            else:
+                if sbj['교과목명'] in ['진리탐구', '공감소통', '사랑실천']:
+                    necessary += int(sbj['학점'])
+                cul += int(sbj['학점'])
+                total += int(sbj['학점'])
+    
+    if major_2 == False:
+        maj_1 = maj_dict[major_1][1] - maj_1
+        maj_r1 = maj_dict[major_1][2] - maj_r1
+        maj_c1 = maj_dict[major_1][3] - maj_c1
+    else:
+        maj_1 = maj_dict[major_1][4] - maj_1
+        maj_r1 = maj_dict[major_1][2] - maj_r1
+        maj_c1 = maj_dict[major_1][3] - maj_c1
+        maj_r2 = maj_dict[major_2][5] - maj_r2
+        maj_c2 = maj_dict[major_2][6] - maj_c2
+    if foreigner == True:
+        cul = 18 - cul
+        necessary = True
+    else:
+        if necessary >= 4:
+            necessary = True
+        else:
+            necessary = False
+        cul = 35 - cul
+    total = maj_dict[major_1][7] - total
+    
+    return [cul, maj_1, maj_r1, maj_c1, maj_2, maj_r2, maj_c2, total, necessary]
+
+
+# 2018 교육과정 (18, 19, 20 학번)
+def course_2018(major_1, major_2, foreigner, records, maj_dict):
+    '''
+    0. cul: 교양(기타) 학점
+    1. maj_1: 본전공 학점
+    2. maj_r1: 본전공 전공필수
+    3. maj_c1: 본전공 선택필수
+    4. maj_2: 복수전공 학점
+    5. maj_r2: 복수전공 전공필수
+    6. maj_c2: 복수전공 선택필수
+    7. total: 총 학점
+    8. necessary: 필수교양 학점
+    '''
+    cul, maj_1, maj_r1, maj_c1, maj_2, maj_r2, maj_c2, total, necessary = 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+    for div, rec in records.items(): # 각 이수구분
+        for sbj in rec: # 각 과목
+            if sbj['유효구분'] != '유효':
+                continue
+            if '전공필수' in div:
+                if div[5:-1] in major_1: # 본전공
+                    maj_r1 += 1
+                    maj_1 += int(sbj['학점'])
+                    total += int(sbj['학점'])
+                elif major_2 != False and div[5:-1] in major_2: # 복수전공
+                    maj_r2 += 1
+                    maj_2 += int(sbj['학점'])
+                    total += int(sbj['학점'])
+                else: # 기타
+                    cul += int(sbj['학점'])
+                    total += int(sbj['학점'])
+            elif '선택필수' in rec:
+                if div[5:-1] in major_1: # 본전공 
+                    maj_c1 += 1
+                    maj_1 += int(sbj['학점'])
+                    total += int(sbj['학점'])
+                elif major_2 != False and div[5:-1] in major_2: # 복수전공
+                    maj_c2 += 1
+                    maj_2 += int(sbj['학점'])
+                    total += int(sbj['학점'])
                 else: # 기타
                     cul += int(sbj['학점'])
                     total += int(sbj['학점'])
@@ -163,9 +242,11 @@ def check(user_id, major_1, major_2, foreigner, records):
 
     if user_id[2:4] in ['12', '13', '14', '15', '16']:
         data = course_2012(major_1, major_2, foreigner, records, maj_dict)
-    if user_id[2:4] == '17':
+    elif user_id[2:4] == '17':
         data = course_2017(major_1, major_2, foreigner, records, maj_dict)
-
+    elif user_id[2:4] in ['18', '19', '20']:
+        data = course_2018(major_1, major_2, foreigner, records, maj_dict)
+    
     if data[8] == False:
         result['필수교양'] = '미이수'
     if data[0] > 0:
